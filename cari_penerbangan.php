5591b9e58f498ebe0a5d32b288c37a3addf5845a
Ahmadfarisy893 <ahmadhuseinalfarisy@gmail.com>
2025-03-26 14:15:58 +0700

    Jumlah penerbangan yang tersedia

---
+++ cari_penerbangan.php
@@ -0,0 +1,162 @@
+<?php
+include (".includes/header.php");
+$title = "cari penerbangan";
+// menyertakan file untuk menampilakn notifikasi (jika ada) 
+include '.includes/toast_notification.php';
+?>
+
+<div class="container">
+              <h1 class="text-center py-3 ">Dalam Negeri</h1>
+              <div class="row">
+              <div class="col-md-6 col-lg-4 mb-3">
+                <div class="card h-100">
+                  <img class="card-img-top" src="assets/img/penerbangan/Photograph by Simon Spring on unsplash.png" alt="Jakarta" height="500"/>
+                    <div class="card-body">
+                      <h5 class="card-title">Jakarta</h5>
+                        <p class="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                      <a href="jakarta.php" class="btn btn-outline-primary">Lihat</a>
+                    </div>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-4 mb-3">
+                <div class="card h-100">
+                  <img class="card-img-top" src="assets/img/penerbangan/View of the evening sky in the city of Batam, to be precise at the Pollux Habibie building.png" alt="Card image cap" height="500"/>
+                    <div class="card-body">
+                      <h5 class="card-title">Batam</h5>
+                        <p class="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                      <a href="javascript:void(0)" class="btn btn-outline-primary">Lihat</a>
+                    </div>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-4 mb-3">
+                <div class="card h-100">
+                  <img class="card-img-top" src="assets/img/penerbangan/Nusa Penida Travel Guide_ How to Visit Nusa Penida from Bali.png" alt="Card image cap" height="500"/>
+                    <div class="card-body">
+                      <h5 class="card-title">Bali</h5>
+                        <p class="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                      <a href="javascript:void(0)" class="btn btn-outline-primary">Lihat</a>
+                    </div>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-4 mb-3">
+                <div class="card h-100">
+                  <img class="card-img-top" src="assets/img/penerbangan/Padang.png" alt="Card image cap" height="500"/>
+                    <div class="card-body">
+                      <h5 class="card-title">Padang</h5>
+                        <p class="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                      <a href="javascript:void(0)" class="btn btn-outline-primary">Lihat</a>
+                    </div>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-4 mb-3">
+                <div class="card h-100">
+                  <img class="card-img-top" src="assets/img/penerbangan/Raja Ampat.png" alt="Card image cap" height="500"/>
+                    <div class="card-body">
+                      <h5 class="card-title">Raja Ampat</h5>
+                        <p class="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                      <a href="javascript:void(0)" class="btn btn-outline-primary">Lihat</a>
+                    </div>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-4 mb-3">
+                <div class="card h-100">
+                  <img class="card-img-top" src="assets/img/penerbangan/Treasure Bay Bintan _  Destinasi _ ExoVillage.png" alt="Card image cap" height="500" />
+                    <div class="card-body">
+                      <h5 class="card-title">Treasure Bay Bintan</h5>
+                        <p class="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                      <a href="javascript:void(0)" class="btn btn-outline-primary">Lihat</a>
+                    </div>
+                </div>
+              </div>
+              
+              <h1 class="text-center py-3">Luar Negeri</h1>
+              <div class="col-md-6 col-lg-4 mb-3">
+                <div class="card h-100">
+                  <img class="card-img-top" src="assets/img/penerbangan/Petronas Towers - Kuala Lumpur, Malaysia.png" alt="Card image cap" height="500"/>
+                    <div class="card-body">
+                      <h5 class="card-title">Malaysia</h5>
+                        <p class="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                      <a href="javascript:void(0)" class="btn btn-outline-primary">Lihat</a>
+                    </div>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-4 mb-3">
+                <div class="card h-100">
+                  <img class="card-img-top" src="assets/img/penerbangan/Singapore.png" alt="Card image cap" height="500"/>
+                    <div class="card-body">
+                      <h5 class="card-title">Singapore</h5>
+                        <p class="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                      <a href="javascript:void(0)" class="btn btn-outline-primary">Lihat</a>
+                    </div>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-4 mb-3">
+                <div class="card h-100">
+                  <img class="card-img-top" src="assets/img/penerbangan/Sunrise at Opera.png" alt="Card image cap" height="500"/>
+                    <div class="card-body">
+                      <h5 class="card-title">Australia</h5>
+                        <p class="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                      <a href="javascript:void(0)" class="btn btn-outline-primary">Lihat</a>
+                    </div>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-4 mb-3">
+                <div class="card h-100">
+                  <img class="card-img-top" src="assets/img/penerbangan/timo-wagner-fT6-YkB0nfg-unsplash.jpg" alt="Card image cap" height="500"/>
+                    <div class="card-body">
+                      <h5 class="card-title">Unite Stage of America</h5>
+                        <p class="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                      <a href="javascript:void(0)" class="btn btn-outline-primary">Lihat</a>
+                    </div>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-4 mb-3">
+                <div class="card h-100">
+                  <img class="card-img-top" src="assets/img/penerbangan/14 Days in Japan_ Japan Golden Route Itinerary For First Timers.png" alt="Card image cap" height="500"/>
+                    <div class="card-body">
+                      <h5 class="card-title">Japan</h5>
+                        <p class="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                      <a href="javascript:void(0)" class="btn btn-outline-primary">Lihat</a>
+                    </div>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-4 mb-3">
+                <div class="card h-100">
+                  <img class="card-img-top" src="assets/img/penerbangan/seoul-tower-red-autumn-maple-leaves-namsan-mountain-south-korea.jpg" alt="Card image cap" height="500"/>
+                    <div class="card-body">
+                      <h5 class="card-title">Korea</h5>
+                        <p class="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                      <a href="javascript:void(0)" class="btn btn-outline-primary">Lihat</a>
+                    </div>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+<?php 
+include (".includes/footer.php");
+?>
