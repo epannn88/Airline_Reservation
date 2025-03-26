44175d38ac27ed22f116320f229e3445b205581c
jefristar17 <jeff711720@gmail.com>
2025-03-26 15:14:39 +0700

    mengubah kode di init session

--- .includes/init_session.php
+++ .includes/init_session.php
@@ -2,9 +2,10 @@
 session_start();
 
 // mengambil data dari session/login
-$penumpang_id = $_SESSION["penumpang_id"];
+$user_id = $_SESSION["user_id"];
 $nama = $_SESSION["nama"];
-$user = $_SESSION["user"];
+$email = $_SESSION["email"];
+$role = $_SESSION["role"];
 // ambil notifikasi jika ada, kemudian hapus dari session/sesi
 $notification = $_SESSION['notification'] ?? null;
 if ('notification') {
@@ -12,7 +13,7 @@ if ('notification') {
 }
 /* periksa apakah session/sesi nama dan user sudah ada,
 jika tidak ada maka arahkan ke halaman login */
-if (empty($_SESSION["nama"]) || empty($_SESSION["user"])) {
+if (empty($_SESSION["nama"]) || empty($_SESSION["role"])) {
     $_SESSION['notification'] = [
         'type' => 'danger',
         'message' => 'Silakan login terlebih dahulu'
