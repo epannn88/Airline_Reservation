270602ddc78e776071275f1bfd0f11c773e2b611
Ahmadfarisy893 <ahmadhuseinalfarisy@gmail.com>
2025-03-26 14:08:24 +0700

    Tampilan sidemenu

--- .includes/sidemenu.php
+++ .includes/sidemenu.php
@@ -1,8 +1,8 @@
 <!-- Menu -->
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
   <div class="app-brand demo">
-    <a href="./dashboard.php" class="app-brand-link">
-      <span class="app-brand-text demo menu-text fw-bolder ms-2 text-uppercase">IdeKreatif</span>
+    <a href="dashboard.php" class="app-brand-link">
+      <span class="app-brand-text demo menu-text fw-bolder ms-0 text-uppercase fs-6 ">Airline Reservation</span>
     </a>
     <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
       <i class="bx bx-chevron-left bx-sm align-middle"></i>
@@ -10,34 +10,47 @@
   </div>
   <div class="menu-inner-shadow"></div>
   <ul class="menu-inner py-1">
-    <!-- Dashboard -->
-    <li class="menu-item">
-      <a href="dashboard.php" class="menu-link">
-        <i class="menu-icon tf-icons bx bx-home-circle"></i>
-        <div data-i18n="Analytics">Dashboard</div>
-      </a>
-    </li>
-    <!-- Forms & Tables -->
-    <li class="menu-header small text-uppercase"><span class="menu-header-text">Posts</span></li>
-    <!-- Forms -->
-    <li class="menu-item">
-      <a href="javascript:void(0);" class="menu-link menu-toggle">
-        <i class="menu-icon tf-icons bx bx-detail"></i>
-        <div data-i18n="Posts">Posts</div>
-      </a>
-      <ul class="menu-sub">
-        <li class="menu-item">
-          <a href="posts.php" class="menu-link">
-            <div data-i18n="Basic Inputs">Post Baru</div>
-          </a>
-        </li>
-        <li class="menu-item">
-          <a href="kategori.php" class="menu-link">
-            <div data-i18n="Input groups">Kategori</div>
-          </a>
-        </li>
-      </ul>
-    </li>
-  </ul>
-</aside>
-<!-- / Menu -->
+  <!-- Dashboard -->
+            <li class="menu-item ">
+              <a href="dashboard.php" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <div data-i18n="Analytics">Dashboard</div>
+              </a>
+            </li>
+            <li class="menu-header small text-uppercase">
+              <span class="menu-header-text">Akun</span>
+            </li>
+            <li class="menu-item">
+                  <a href="akun.php" class="menu-link">
+                  <i class="menu-icon tf-icons bx bx-user-circle"></i>
+                    <div data-i18n="Account">Account</div>
+                  </a>
+            </li>
+
+
+            <!-- Menu Baru untuk Fitur Penerbangan -->
+            <li class="menu-header small text-uppercase"><span class="menu-header-text">Penerbangan</span></li>
+            <li class="menu-item">
+              <a href="cari_penerbangan.php" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-search-alt"></i>
+                <div data-i18n="Cari Penerbangan">Cari Penerbangan</div>
+              </a>
+            </li>
+            <li class="menu-item">
+              <a href="booking.php" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-briefcase"></i>
+                <div data-i18n="Pemesanan Saya">Pemesanan Saya</div>
+              </a>
+            </li>
+            
+            <!-- Menu Promo dan Voucher -->
+            <li class="menu-item">
+              <a href="promosi.php" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-gift"></i>
+                <div data-i18n="Promo & Voucher">Promo & Voucher</div>
+              </a>
+            </li>
+            
+          </ul>
+        </aside>
+        <!-- / Menu -->
