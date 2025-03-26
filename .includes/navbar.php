83324c98f719f1bb15eb38b2f6dab8ba2b1c8667
jefristar17 <jeff711720@gmail.com>
2025-03-26 15:09:18 +0700

    menambahkan sedikit kode di navbar

--- .includes/navbar.php
+++ .includes/navbar.php
@@ -1,3 +1,11 @@
+<?php
+require "config.php";
+
+$_SESSION["id"] = 1;
+$_sessionId = $_SESSION["id"];
+$img = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM tb_user WHERE id = $_sessionId"));
+?>
+
 <!-- Navbar -->
 <nav class="layout-navbar container-xxl navbar navbar-expand-xl
 navbar-detached align-items-center bg-navbar-theme" id="layout-navbar">
@@ -12,7 +20,12 @@ navbar-detached align-items-center bg-navbar-theme" id="layout-navbar">
       <li class="nav-item navbar-dropdown dropdown-user dropdown">
         <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
           <div class="avatar avatar-online">
-            <img src="assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+          <?php
+            $id = $img["id"];
+            $name = $img["name"];
+            $image = $img["image"];
+          ?>
+          <img src=<?php echo $image; ?> width= 125 height= 125  class="w-px-40 h-auto rounded-circle"/>
           </div>
         </a>
         <ul class="dropdown-menu dropdown-menu-end">
@@ -21,12 +34,12 @@ navbar-detached align-items-center bg-navbar-theme" id="layout-navbar">
               <div class="d-flex">
                 <div class="flex-shrink-0 me-3">
                   <div class="avatar avatar-online">
-                    <img src="assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+                  <img src=<?php echo $image; ?> width= 125 height= 125  class="w-px-40 h-auto rounded-circle"/>
                   </div>
                 </div>
                 <!-- nama -->
                 <div class="flex-grow-1">
-                  <span class="fw-semibold d-block"><?php echo $name; ?></span>
+                  <span class="fw-semibold d-block"><?php echo $nama; ?></span>
                   <small class="text-muted"><?php echo $role; ?></small>
                 </div>
               </div>
