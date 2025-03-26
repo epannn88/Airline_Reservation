e6900de0dc908f215c7f5b880cb61e7729fa9d8e
jefristar17 <jeff711720@gmail.com>
2025-03-26 15:27:44 +0700

    mengubah sedikit kode di login auth

--- auth/login_auth.php
+++ auth/login_auth.php
@@ -2,34 +2,44 @@
 session_start(); // memulai sesi
 require_once("../config.php");
 
-// proses login
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email_nama = $_POST["email_nama"];
     $password = $_POST["password"];
     
     $sql = mysqli_query($conn, 
-    "SELECT * FROM penumpang WHERE (nama = '$email_nama' OR email = '$email_nama') 
-    AND password = '$password'");
+    "SELECT * FROM users WHERE nama = '$email_nama' OR email = '$email_nama'");
 
     if (mysqli_num_rows($sql)> 0) {
         $row = mysqli_fetch_array($sql);
-        $_SESSION['penumpang_id'] = $row['penumpang_id'];
+
+        if (password_verify($password, $row['password'])) {
+        $_SESSION['user_id'] = $row['user_id'];
         $_SESSION['nama'] = $row['nama'];
-        $_SESSION['user'] = $row['user'];
-            // simpan notifikasi kedalam session
+        $_SESSION['email'] = $row['email'];
+        $_SESSION['role'] = $row['role'];
+        
             $_SESSION['notification'] = [
                 'type' => 'primary',
                 'message' => 'Registrasi berhasil!'
             ];
         header('Location: ../dashboard.php');
+        exit();
         } else {
             $_SESSION['notification'] = [
                 'type' => 'danger',
-                'message' => 'Gagal registrasi: ' . mysqli_error($conn)
+                'message' => 'Password salah'
             ];
         header('Location: login.php');
         exit();
-    }
+        }
+        } else {
+            $_SESSION['notification'] = [
+                'type' => 'danger',
+                'message' => 'Nama atau email salah'
+            ];
+        }
+        header('Location: login.php');
+        exit();
 }   
 $conn->close();
 ?>
