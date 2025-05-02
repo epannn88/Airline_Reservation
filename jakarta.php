d8edb73e7e31478f934d308c74a2d8eecb7afe72
Ahmadfarisy893 <ahmadhuseinalfarisy@gmail.com>
2025-05-02 13:28:58 +0700

    ditambahkannya option

--- jakarta.php
+++ jakarta.php
@@ -65,6 +65,13 @@ include '.includes/toast_notification.php';
  <div class="card mx-4 mt-4">
                 <div class="card-header">
                   <div class="table-responsive text-nowrap">
+                  <select id="filterAsal" class="form-select me-2" style="width: 150px;">
+                    <option value="">Filter Berdasarkan Asal</option>
+                    <option value="">jakarta</option>
+                    <option value="">batam</option>
+                    <option value="">dan lain</option>
+                    <!-- option ini digunakan untuk memfilter penerbangan -->
+                </select>
                     <table id="testdatatable" class="table table-hover">
                       <thead>
                         <tr>
@@ -76,56 +83,42 @@ include '.includes/toast_notification.php';
                           <th>Harga</th>
                           <th>Actions</th>
                           <th>More</th>
+                          <!-- tabel header -->
                         </tr>
                       </thead>
                       <tbody class="table-border-bottom-0">
                         <?php 
                         $index = 1;
-
+                        // data yang akan ditampilkan akan selalu dimulai dari angka 1
                         $query = $conn->query("SELECT * FROM penerbangan");
+                        // mengambil semua dari tabel penerbangan
                         while ($penerbangan = $query->fetch_assoc()) {
+                          // fetch_assoc() Mengambil data penerbangan per baris
+                          // data yang di ambil akan di ubah menjadi array 
+                          // while memastikan Selalu ulangi proses selama masih ada data penerbangan.
                         ?>
                         <tr>
                           <td><?= $index++; ?></td>
+                          <!-- data pertama = 1 data kedua = 2 -->
                           <td><?= $penerbangan['kota_asal']; ?></td>
                           <td><?= $penerbangan['kota_tujuan']; ?></td>
                           <td><?= $penerbangan['maskapai']; ?></td>
                           <td><?= $penerbangan['jam_penerbangan']; ?></td>
                           <td><?= $penerbangan['harga']; ?></td>
-                          <td><a href="pemesanan.php" class="btn rounded-pill btn-outline-success">lanjutkan</td>
+                          <!--menampilkan kota_asal, kota_tujuan, maskapai, jam_penerbangan, harga -->
+                          <td><a href="add_penumpang.php" class="btn rounded-pill btn-outline-success">lanjutkan</td>
 
                           <td>
-                            <a href="jakarta_edit.php?id_penerbangan=<?php echo $penerbangan['id_penerbangan'] ?>" class="btn rounded-pill btn-outline-success">edit </a>
-                            <a onclick="return confirm('Anda yakin ingin menghapus data?')"
-                            href="jakarta_process.php?id_penerbangan=<?php echo $penerbangan['id_penerbangan'] ?>" class="btn rounded-pill btn-outline-success">hapus</a>
-
+                            <a href="jakarta_edit.php?penerbangan_id=<?php echo $penerbangan['penerbangan_id'] ?>" class="btn rounded-pill btn-outline-success">edit </a>
+                            <a onclick="return confirm('Anda yakin ingin menghapus data?')" 
+                            href="jakarta_process.php?penerbangan_id=<?php echo $penerbangan['penerbangan_id'] ?>" class="btn rounded-pill btn-outline-success">hapus</a>
+                            <!-- Ketika tombol hapus di tekan maka akan muncul pesan 'Anda yakin ingin menghapus data?'
+                             setelah itu data akan di hapus di jakarta_proses -->
                           </td>
                         <?php
                         }
                         ?>
                     </tbody>
-
-                    <div class="modal fade" id="deletePost_<?= $penerbang['id_penerbangan']; ?>" tabindex="-1" aria-hidden="true">
-                                        <div class="modal-dialog" role="document">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h5 class="modal-title">Hapus Post?</h5>
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <form action="jakarta_edit_process.php" method="POST">
-                                                        <p>Tindakan ini tidak bisa dibatalkan.</p>
-                                                        <input type="hidden" name="postID" value="<?= $post['id_penerbangan']; ?>">
-                                                        <div class="modal-footer">
-                                                            <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Batal</button>
-                                                            <button type="submit" name="delete" class="btn btn-primary">Hapus</button>
-                                                        </div>
-                                                    </form>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
                 </table>
                 <div  class="d-flex justify-content-end mx-5">
                     <a href="tambahPenerbangan.php" class="add-button">
