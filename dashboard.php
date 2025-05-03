cadb8fb418be7b573b0c8ca7b93e91b25429480c
Ahmadfarisy893 <ahmadhuseinalfarisy@gmail.com>
2025-05-03 15:00:06 +0700

    Tampilan Dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,7 @@
 <?php
+//include header agar header dapat ditampilkan
 include (".includes/header.php");
-$title = "Dashboard";
+$title = "Dashboard"; // nama halaman
 // menyertakan file untuk menampilakn notifikasi (jika ada) 
 include '.includes/toast_notification.php';
 ?>
@@ -16,18 +17,19 @@ include '.includes/toast_notification.php';
                       <div class="col-sm-7">
                         <div class="card-body">
                           <h5 class="card-title text-primary">Selamat Datang <?php echo $nama; ?>! 🎉</h5>
-                          <p class="mb-4">
-                            You have done <span class="fw-bold">72%</span> more sales today. Check your new badge in
-                            your profile.
+                          <p class="mb-3 text-justify">
+                            Selamat Datang di Airline Reservation <?php echo $nama; ?>! Airline Reservation menyediakan layanan pemesanan tiket penerbangan domestik dan internasional dengan 
+                            <span class="fw-bold">kemudahan, kecepatan,</span> dan <span class="fw-bold">harga terbaik</span>!
+                          </p> 
                           </p>
-                          <a href="javascript:;" class="btn btn-sm btn-outline-primary">View Badges</a>
+                          <a href="cari_penerbangan.php" class="btn btn-m btn-outline-primary ">Lihat Penerbangan</a>
                         </div>
                       </div>
                       <div class="col-sm-5 text-center text-sm-left">
                         <div class="card-body pb-0 px-0 px-md-4">
                           <img
-                            src="assets/img/illustrations/man-with-laptop-light.png"
-                            height="140"
+                            src="assets/img/illustrations/Departing Customizable Flat Illustrations _ Rafiki Style.png"
+                            height="220"
                             alt="View Badge User"
                             data-app-dark-img="illustrations/man-with-laptop-dark.png"
                             data-app-light-img="illustrations/man-with-laptop-light.png"
@@ -52,7 +54,7 @@ include '.includes/toast_notification.php';
                     <div class="card-body">
                       <h5 class="card-title">Jakarta</h5>
                         <p class="card-text">
-                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                        Ibu kota Indonesia yang moderen
                         </p>
                       <a href="jakarta.php" class="btn btn-outline-primary">Lihat</a>
                     </div>
@@ -64,7 +66,7 @@ include '.includes/toast_notification.php';
                     <div class="card-body">
                       <h5 class="card-title">Batam</h5>
                         <p class="card-text">
-                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                        Pulau strategis di Selat Malaka dengan keindahan yang laut menakjubkan.
                         </p>
                       <a href="javascript:void(0)" class="btn btn-outline-primary">Lihat</a>
                     </div>
@@ -76,14 +78,14 @@ include '.includes/toast_notification.php';
                     <div class="card-body">
                       <h5 class="card-title">Bali</h5>
                         <p class="card-text">
-                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                        Pantai eksotis, sawah terasering Ubud, pura megah, dan kehidupan malam semarak di Seminyak.
                         </p>
                       <a href="javascript:void(0)" class="btn btn-outline-primary">Lihat</a>
                     </div>
                 </div>
               </div>
               <div class="d-grid gap-2 col-lg-6 mx-auto mb-5 mt-3">
-                <button class="btn btn-primary btn-lg" type="button">View More</button>
+                <a href="cari_penerbangan.php" class="btn btn-primary btn-lg" type="button">View More</a>
               </div>
               <h1 class="text-center py-3">Luar Negeri</h1>
               <div class="col-md-6 col-lg-4 mb-3">
@@ -123,7 +125,7 @@ include '.includes/toast_notification.php';
                 </div>
               </div>
               <div class="d-grid gap-2 col-lg-6 mx-auto mb-5 mt-3">
-                <button class="btn btn-primary btn-lg" type="button" >View More</button>
+                <a href="cari_penerbangan.php" class="btn btn-primary btn-lg" type="button" >View More</a>
               </div>
             </div>
           </div>
