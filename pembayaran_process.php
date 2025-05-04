853d84a6c36cb7f5bf94d7d1d2ea125b9da2b11e
jefristar17 <jeff711720@gmail.com>
2025-05-04 20:04:09 +0700

    tambah di bagian pembayaran process

--- pembayaran_process.php
+++ pembayaran_process.php
@@ -8,6 +8,7 @@ $email = $_POST['email'];
 $biaya = $_POST['biaya'];
 $order_id= rand();
 $transaction_status= 1;
+$status_pembayaran = "Pembayaran Berhasil";
 
 // menginput data ke database
 mysqli_query($conn, "insert into pembayaran values('','$nama','$email','$biaya','$order_id','$transaction_status','$status_pembayaran')");
